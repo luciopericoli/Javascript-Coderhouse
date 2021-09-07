25e6caceb4ccdfa8959a0ee8550be471b7110192
Lucio Pericoli <luciopericoli@gmail.com>
2021-09-07 19:34:14 -0300

    desafio 1

---
+++ lucio.js
@@ -0,0 +1,16 @@
+let hablar="Hola lucio"
+console.log(hablar) 
+alert (hablar)
+
+let entrada = prompt("Ingrese su equipo");
+let salida = entrada + " " + "gano";
+alert(salida);
+console.log(salida);
+
+let numeroA = 10;
+let numeroB = 33;
+let numeroC = 50;
+
+let resultadoSuma = numeroA + numeroC;
+let resultadoResta = numeroB + numeroA;
+console.log(resultadoSuma);
