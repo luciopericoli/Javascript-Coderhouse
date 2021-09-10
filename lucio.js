213029a515a9bdd415b88058a984068d0cd25461
Lucio Pericoli <luciopericoli@gmail.com>
2021-09-09 21:27:51 -0300

    desafio 2

--- lucio.js
+++ lucio.js
@@ -14,3 +14,19 @@ let numeroC = 50;
 let resultadoSuma = numeroA + numeroC;
 let resultadoResta = numeroB + numeroA;
 console.log(resultadoSuma);
+
+let nombreApellido = prompt ("Ingrese su nombre y apellido");
+
+if (nombreApellido == ""){
+    alert("Ingrese nuevamente los datos requeridos");
+}
+else {
+    alert ("Nombre y apellido: " + nombreApellido);
+}
+
+let nombreYApellido = prompt ("Ingrese su nombre y apellido");
+if ((nombreYApellido !="") && ((nombreYApellido == "Lucio Pericoli") || (nombreYApellido == "lucio pericoli"))){
+    alert ("Hola Lucio Pericoli");
+} else {
+    alert("Error: Ingrese sus datos nuevamente")
+}
