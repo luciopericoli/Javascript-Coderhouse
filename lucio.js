f32c137e32adc0064d82c47dc5af862de203528c
Lucio Pericoli <luciopericoli@gmail.com>
2021-09-14 19:53:51 -0300

    bucle

--- lucio.js
+++ lucio.js
@@ -1,32 +1,18 @@
-let hablar="Hola lucio"
-console.log(hablar) 
-alert (hablar)
-
 let entrada = prompt("Ingrese su equipo");
-let salida = entrada + " " + "gano";
-alert(salida);
-console.log(salida);
 
-let numeroA = 10;
-let numeroB = 33;
-let numeroC = 50;
-
-let resultadoSuma = numeroA + numeroC;
-let resultadoResta = numeroB + numeroA;
-console.log(resultadoSuma);
-
-let nombreApellido = prompt ("Ingrese su nombre y apellido");
-
-if (nombreApellido == ""){
-    alert("Ingrese nuevamente los datos requeridos");
-}
-else {
-    alert ("Nombre y apellido: " + nombreApellido);
-}
-
-let nombreYApellido = prompt ("Ingrese su nombre y apellido");
-if ((nombreYApellido !="") && ((nombreYApellido == "Lucio Pericoli") || (nombreYApellido == "lucio pericoli"))){
-    alert ("Hola Lucio Pericoli");
-} else {
-    alert("Error: Ingrese sus datos nuevamente")
+while(entrada != "ESC" ){
+   switch (entrada) {
+       case "Tigre":
+            alert("Campeon 2019");
+            break;
+        case "Rosario central":
+            alert("Campeon 1986/87");
+            break;
+        case "Racing":
+            alert("?")
+       default:
+           alert("Saliste campeon?")
+           break;
+   }
+   entrada = prompt("Ingresar su equipo");
 }
