c6a399b67eb2d790f19d627cd715c7252ec90a3a
Lucio Pericoli <luciopericoli@gmail.com>
2021-09-16 19:36:36 -0300

    Update lucio.js

--- lucio.js
+++ lucio.js
@@ -1,18 +1,18 @@
-let entrada = prompt("Ingrese su equipo");
-
-while(entrada != "ESC" ){
-   switch (entrada) {
-       case "Tigre":
-            alert("Campeon 2019");
-            break;
-        case "Rosario central":
-            alert("Campeon 1986/87");
-            break;
-        case "Racing":
-            alert("?")
-       default:
-           alert("Saliste campeon?")
-           break;
-   }
-   entrada = prompt("Ingresar su equipo");
+//Declaración de variable para guardar el resultado de la suma
+let resultado = 0;
+//Función que suma dos números y asigna a resultado
+function sumar(primerNumero, segundoNumero) {
+    resultado = primerNumero + segundoNumero;
 }
+//Función que muestra resultado por consola
+function mostrar(mensaje) {
+    console.log(mensaje);
+}
+//Llamamos primero a sumar y luego a mostrar
+sumar(6, 3);            
+mostrar(resultado); 
+
+function sumar(primerNumero, segundoNumero) {
+    return primerNumero + segundoNumero;
+}
+let resultado = sumar(5, 8);
