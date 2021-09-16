68d14e9567b19c8827178cd2a192186ed35b1d69
Lucio Pericoli <luciopericoli@gmail.com>
2021-09-16 20:14:26 -0300

    desafio clase 5

--- lucio.js
+++ lucio.js
@@ -1,18 +1,34 @@
-//Declaración de variable para guardar el resultado de la suma
-let resultado = 0;
-//Función que suma dos números y asigna a resultado
-function sumar(primerNumero, segundoNumero) {
-    resultado = primerNumero + segundoNumero;
-}
-//Función que muestra resultado por consola
-function mostrar(mensaje) {
-    console.log(mensaje);
-}
-//Llamamos primero a sumar y luego a mostrar
-sumar(6, 3);            
-mostrar(resultado); 
+const suma  = (a,b) => a + b;
+const resta = (a,b) => a - b;
 
-function sumar(primerNumero, segundoNumero) {
-    return primerNumero + segundoNumero;
-}
-let resultado = sumar(5, 8);
+const iva   = x => x * 0.21;
+let precioProducto  = 1500; 
+let precioDescuento = 150;  
+
+let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), precioDescuento); 
+console.log(nuevoPrecio);
+
+let comidadeperro = prompt("¿Que producto quisiera agregar?")
+let accesorio = 1300
+
+alert("Su producto es","comidadeperro")
+
+let compra = iva(suma(comidadeperro,accesorio))
+
+
+
+while(entrada != "ESC" ) entrada = prompt("Ingresar su comida de perro");
+    switch (entrada) {
+        case "Vital Can balanced":
+             alert("$1.500");
+             break;
+         case "Eukanuba cachorros":
+             alert("$2.800");
+             break;
+         case "Royal canin mini":
+             alert("$8.000")
+        default:
+            alert("Busque su producto")
+            break;
+    }
+    
